98e6bcefc4da4f4a90ea00bc43a8c93ceb96db74
Paweł Stawicki <stawel+pi@gmail.com>
2019-05-13 00:11:44 +0200

    Update server

--- server/stream.py
+++ server/stream.py
@@ -7,7 +7,21 @@ import picamera
 import logging
 import socketserver
 from threading import Condition
+import threading
+import time
 from http import server
+from fractions import Fraction
+
+#resolution = (640, 480)
+#resolution = (2592, 1944)
+#resolution = (1296, 730)
+#resolution = (1296, 972)
+
+resolution = (2592/2, 200)
+shutter_speed = 65759
+awb_gains = (Fraction(139, 128), Fraction(227, 128))
+
+awb_gains = (1, 1)
 
 PAGE="""\
 <html>
@@ -16,7 +30,7 @@ PAGE="""\
 </head>
 <body>
 <center><h1>Raspberry Pi - spectrometer </h1></center>
-<center><img src="stream.mjpg" width="640" height="480"></center>
+<center><img src="stream.mjpg"></center>
 </body>
 </html>
 """
@@ -77,16 +91,59 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.send_error(404)
             self.end_headers()
 
+def printInfo(camera):
+    print('iso: ', camera.iso)
+    print('analog_gain: ', camera.analog_gain)
+    print('digital_gain: ', camera.digital_gain)
+    print('awb_gains: ', camera.awb_gains)
+    print('awb_mode: ', camera.awb_mode)
+    print('brightness: ', camera.brightness)
+    print('clock_mode: ', camera.clock_mode)
+    print('contrast: ', camera.contrast)
+    print('drc_strength: ', camera.drc_strength)
+    print('exposure_compensation: ', camera.exposure_compensation)
+    print('exposure_mode: ', camera.exposure_mode)
+    print('exposure_speed: ', camera.exposure_speed)
+    print('framerate: ', camera.framerate)
+    print('framerate_delta: ', camera.framerate_delta)
+    print('framerate_range: ', camera.framerate_range)
+    print('hflip: ', camera.hflip)
+#    print('led: ', camera.led)
+    print('sensor_mode: ', camera.sensor_mode)
+    print('sharpness: ', camera.sharpness)
+    print('shutter_speed: ', camera.shutter_speed)
+    print('vflip: ', camera.vflip)
+    print('video_denoise: ', camera.video_denoise)
+
+
 class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+with picamera.PiCamera(resolution='%dx%d'%resolution , framerate=15) as camera:
     output = StreamingOutput()
+    camera.iso = 800
+    print('waiting for gain to settle')
+    time.sleep(2)
+    print('runing record')
+    camera.exposure_mode = 'off'
+    camera.shutter_speed = shutter_speed
+    camera.awb_mode = 'off'
+    camera.awb_gains = awb_gains
+    camera.video_denoise = False
     camera.start_recording(output, format='mjpeg')
     try:
         address = ('', 8000)
         server = StreamingServer(address, StreamingHandler)
-        server.serve_forever()
+#        server.serve_forever()
+        ServerThread = threading.Thread(target=server.serve_forever)
+        ServerThread.start()
+#        led = True
+        while True:
+            printInfo(camera)
+            time.sleep(1)
+#            camera.led = led
+#            led = not led
     finally:
         camera.stop_recording()
+        server.shutdown()
